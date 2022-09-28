df24531f3afe08aa0f5e91aab20cddc95f4be8ba
Nuuteen <102475649+Nuuteen@users.noreply.github.com>
2022-09-28 16:23:34 +0700

    Final Lab10

---
+++ flutter_application_1/lib/menu.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Searchmenu extends StatelessWidget {
+  const Searchmenu({key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          centerTitle: true,
+          title: Text("เลือกเมนูอาหาร"),
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.logout), onPressed: () {}),
+          ]),
+      body: Menu(),
+    );
+  }
+}
+
+class Menu extends StatefulWidget {
+  const Menu({Key key}) : super(key: key);
+
+  @override
+  State<Menu> createState() => _MenuState();
+}
+
+class _MenuState extends State<Menu> {
+  List<List<dynamic>> foodLists = [
+    [
+      "https://i.pinimg.com/564x/ce/70/65/ce706553e3e6f4373f953148ed5e6826.jpg",
+      "Hamburger",
+      159
+    ],
+    [
+      "https://i.pinimg.com/564x/40/4e/b5/404eb50e049e33459e8493277919e741.jpg",
+      "French fries",
+      99
+    ],
+    [
+      "https://i.pinimg.com/564x/8a/10/00/8a100000f40a519cd068c7549f792f54.jpg",
+      "CoCa.Cola",
+      15
+    ],
+    [
+      "https://i.pinimg.com/564x/17/ee/0d/17ee0d1c0b91570534f8b38ea1438ced.jpg",
+      "Pizza Hawaiian",
+      259
+    ],
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: foodLists.length,
+      itemBuilder: (_, index) {
+        return Card(
+          elevation: 10,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          child: Container(
+            height: 100,
+            width: double.infinity,
+            child: Stack(
+              children: [
+                Row(
+                  children: [
+                    Image.network(
+                      foodLists[index][0],
+                      fit: BoxFit.cover,
+                      width: 100,
+                    ),
+                    Expanded(
+                      child: Container(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "${foodLists[index][1]}",
+                              style: TextStyle(
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Text(
+                              "ราคาอาหาร  ${foodLists[index][2]} ฿",
+                              style: TextStyle(
+                                color: Colors.green,
+                                fontSize: 18,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                  alignment: Alignment.bottomRight,
+                  child: Chip(
+                    backgroundColor: Colors.amberAccent,
+                    label: Text(
+                      "Oder Now",
+                      style: TextStyle(color: Colors.white, fontSize: 11),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
---
+++ flutter_application_1/lib/test.dart
@@ -0,0 +1,114 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/foodmenu.dart';
+import 'package:flutter_application_1/login_page/login_page.dart';
+import 'package:flutter_application_1/math.dart';
+import 'package:flutter_application_1/menu.dart';
+import 'package:flutter_application_1/secon.dart';
+
+void main() => runApp(const IndexApp());
+
+class IndexApp extends StatelessWidget {
+  const IndexApp({key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+            centerTitle: true,
+            title: Text("INDEX MENU"),
+            leading: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.home),
+            ),
+            actions: <Widget>[
+              IconButton(
+                  icon: Icon(Icons.logout),
+                  onPressed: () {
+                    FirebaseAuth.instance.signOut().then((value) {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) {
+                        return LoginPage();
+                      }));
+                    });
+                  }),
+            ]),
+        body: const MyStatefulWidget(),
+      ),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({key});
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  @override
+  Widget build(BuildContext context) {
+    final style = ButtonStyle(
+        backgroundColor:
+            MaterialStateProperty.all(Color.fromARGB(255, 70, 200, 161)),
+        padding: MaterialStateProperty.all(const EdgeInsets.all(30)),
+        textStyle: MaterialStateProperty.all(const TextStyle(fontSize: 25)));
+    BorderRadius.circular(100);
+    elevation:
+    10;
+
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          ElevatedButton(
+            style: style,
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SecondRoute()),
+              );
+            },
+            child: const Text('     โปรแกรมนับเลข     '),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            style: style,
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Searchmenu2()),
+              );
+            },
+            child: const Text('         เมนูอาหาร          '),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            style: style,
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Calculatorhmenu()),
+              );
+            },
+            child: const Text('      เครื่องคิดเลข        '),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            style: style,
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Searchmenu()),
+              );
+            },
+            child: const Text('     เลือกเมนูอาหาร      '),
+          ),
+        ],
+      ),
+    );
+  }
+}
